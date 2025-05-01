c085f936bc18363549ed5e00e149fa94c479f416
Simonjs <simonjs@live.dk>
2025-05-01 14:31:57 +0200

    you can now press the button slightly early

--- Assets/Simonjs/Scripts/BeatBlock.cs
+++ Assets/Simonjs/Scripts/BeatBlock.cs
@@ -7,16 +7,35 @@ public class BeatBlock : MonoBehaviour
     public Vector3 destination;
     public float destinationBeat;
     public float travelTime = 5;
+    public float buttonTimeoutTime = 20f;
+    private float lastPress;
+    private bool playerPressedButton 
+    { get
+        {
+            Debug.Log(lastPress >= Time.time - buttonTimeoutTime);
+            return lastPress >= Time.time - buttonTimeoutTime;
+           
+        } 
+    }
     private void Start()
     {
         origin = transform.position;
         
         
     }
-    private bool hitBottom = false;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            lastPress = Time.time;
+            
+        }
+        
+    }
+    private bool doLateUpdate = true;
     private void LateUpdate()
     {
-        if (hitBottom) return;
+        if (!doLateUpdate) return;
         float secPerBeat = BeatManager.instance.secPerBeat;
         float currentTime = BeatManager.instance.songCurrentBeat * secPerBeat;
 
@@ -25,25 +44,27 @@ public class BeatBlock : MonoBehaviour
         transform.position = Vector3.Lerp(origin, destination, (currentTime-startTime)/travelTime);
         if ((currentTime - startTime) / travelTime >= 1) 
         {
-            hitBottom = true;
+           
             StartCoroutine(EndBlock());
-
+            doLateUpdate = false;
         }
         
 
     }
+
+   
     public IEnumerator EndBlock()
     {
-        bool playerHasClicked = false;
-        float blockDeathTime = Time.time+0.25f;
-        while (!playerHasClicked&&Time.time<blockDeathTime)
+        
+        float blockDeathTime = Time.time+0.1f;
+        while (!playerPressedButton&&Time.time<blockDeathTime)
         {
-            Debug.Log(Time.time);
-            playerHasClicked = playerHasClicked || Input.GetKey(KeyCode.Mouse0);
-            if (playerHasClicked) GetComponent<MeshRenderer>().material.color = Color.green;
+            
+            
+            if (playerPressedButton) GetComponent<MeshRenderer>().material.color = Color.green;
             yield return new WaitForSeconds(0);
         }
-        GetComponent<MeshRenderer>().material.color = playerHasClicked ? Color.green:Color.red;
+        GetComponent<MeshRenderer>().material.color = playerPressedButton ? Color.green:Color.red;
         yield return new WaitForSeconds(0.25f);
         Destroy(gameObject);
     }
