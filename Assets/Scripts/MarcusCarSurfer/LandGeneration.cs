395d7b617c7874a9c497330af02ec1deb79d9a3c
marcusjs29 <fw56pz@student.aau.dk>
2025-05-12 11:44:30 +0200

    Disabled Landgen,, vi brugte det ikke 🍉

--- Assets/Scripts/MarcusCarSurfer/LandGeneration.cs
+++ Assets/Scripts/MarcusCarSurfer/LandGeneration.cs
@@ -1,41 +1,41 @@
-using UnityEngine;
+//using UnityEngine;
 
-public class LandGenerator : MonoBehaviour
-{
-    public GameObject landSegmentPrefab;
-    public int segmentCount = 10;
+//public class LandGenerator : MonoBehaviour
+//{
+//    public GameObject landSegmentPrefab;
+//    public int segmentCount = 10;
 
-    private Transform lastEndPoint;
+//    private Transform lastEndPoint;
 
-    void Start()
-    {
-        GenerateLand();
-    }
+//    void Start()
+//    {
+//        GenerateLand();
+//    }
 
-    void GenerateLand()
-    {
-        for (int i = 0; i < segmentCount; i++)
-        {
-            GameObject segment = Instantiate(landSegmentPrefab);
+//    void GenerateLand()
+//    {
+//        for (int i = 0; i < segmentCount; i++)
+//        {
+//            GameObject segment = Instantiate(landSegmentPrefab);
 
-            if (lastEndPoint == null)
-            {
-                // First segment at origin
-                segment.transform.position = transform.position;
-            }
-            else
-            {
-                // Place the new segment at the previous one's EndPoint
-                segment.transform.position = lastEndPoint.position;
-            }
+//            if (lastEndPoint == null)
+//            {
+//                // First segment at origin
+//                segment.transform.position = transform.position;
+//            }
+//            else
+//            {
+//                // Place the new segment at the previous one's EndPoint
+//                segment.transform.position = lastEndPoint.position;
+//            }
 
-            // Find the EndPoint child in the new segment
-            lastEndPoint = segment.transform.Find("EndPoint");
-            if (lastEndPoint == null)
-            {
-                Debug.LogError("Prefab must have a child named 'EndPoint'");
-                break;
-            }
-        }
-    }
-}
+//            // Find the EndPoint child in the new segment
+//            lastEndPoint = segment.transform.Find("EndPoint");
+//            if (lastEndPoint == null)
+//            {
+//                Debug.LogError("Prefab must have a child named 'EndPoint'");
+//                break;
+//            }
+//        }
+//    }
+//}
